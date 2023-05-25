81c2f67ed5904e428eefb7c0f9816c197f9e378d
jasonliang910304 <124699451+jasonliang910304@users.noreply.github.com>
2023-05-25 14:16:28 +0800

    Delete bound_dataset.py

--- bound_dataset.py
+++
@@ -1,4 +0,0 @@
-from roboflow import Roboflow
-rf = Roboflow(api_key="1YfKPnmgH56zBY9QL8M5")
-project = rf.workspace("car-test").project("car_bound_dection")
-dataset = project.version(1).download("yolov8")
