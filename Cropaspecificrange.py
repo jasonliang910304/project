318abc6c77dcf6532e5a4b2363e3332d822e38de
shane12345870 <128160732+shane12345870@users.noreply.github.com>
2023-06-29 16:48:46 +0800

    程式更新(切割圖片並顯示座標

--- Cropaspecificrange.py
+++ Cropaspecificrange.py
@@ -1,36 +1,41 @@
 from ultralytics import YOLO
 import cv2
+from matplotlib import pyplot as plt
 
-model = YOLO("yolov8n.pt")  # 載入模型
-results = model("car.jpg")  # 將car.jpg丟進result
-img = cv2.imread("car.jpg")  # 將car.jpg載入cv2，並儲存為img
+model = YOLO("yolov8n.pt")  # 载入模型
+results = model("car5.jpg")  # 将car.jpg丢进result
+img = cv2.imread("car5.jpg")  # 将car.jpg载入cv2，并存储为img
 crop = None  # 初始化用于保存边界框的变量
 
 for result in results:
-    boxes = result.boxes.cpu().numpy()  # 從GPU（如果存在）移動到CPU，並將其轉換为NumPy數組。
-    if len(boxes) > 0:  # 只保存一個邊界框
-        box = boxes[0]  # 從邊界框数组中提取第一個邊界框
-        r = box.xyxy[0].astype(int)  # 並將邊界框儲存至r這個變數
+    boxes = result.boxes.cpu().numpy()  # 从GPU（如果存在）移动到CPU，并将其转换为NumPy数组。
+    if len(boxes) > 0:  # 只保存一个边界框
+        box = boxes[0]  # 从边界框数组中提取第一个边界框
+        r = box.xyxy[0].astype(int)  # 并将边界框存储至r这个变量
         crop = img[
             r[1] : r[3], r[0] : r[2]
-        ]  # 圖片切割:r[1] 是矩形的上邊界座標，r[3]是矩形的下邊界座標，r[0] 是矩形的左邊界座標，r[2]是矩形的右邊界座標。
-        cv2.imwrite("cropped.jpg", crop)  # 將CROP存為JPG檔的圖片
-        # 打印邊界框座標
+        ]  # 图片切割:r[1] 是矩形的上边界坐标，r[3]是矩形的下边界坐标，r[0] 是矩形的左边界坐标，r[2]是矩形的右边界坐标。
+        cv2.imwrite("cropped.jpg", crop)  # 将CROP存为JPG文件的图像
+        # 打印边界框坐标
         print("Box Coordinates:", r)
 
-        # 在圖像視窗顯示出座標值
-        cv2.rectangle(img, (r[0], r[1]), (r[2], r[3]), (0, 255, 0), 2)  # 畫出矩形邊界
-        # cv2.putText() 函數將位於矩形框上方的文字繪製在圖片上，以顯示邊界框的座標。
+        # 在图像上绘制边界框和文本
+        cv2.rectangle(img, (r[0], r[1]), (r[2], r[3]), (0, 255, 0), 2)  # 画出矩形边界
         cv2.putText(
             img,
             f"Box: ({r[0]}, {r[1]}) ({r[2]}, {r[3]})",  # 文字信息
-            (r[0], r[1] - 10),  # 繪製邊框的起始位置
-            cv2.FONT_HERSHEY_SIMPLEX,  # 字體
-            0.9,  # 字體大小
-            (0, 255, 0),  # 字體顏色
-            2,  # 字體粗細
+            (r[0], r[1] - 10),  # 绘制边框的起始位置
+            cv2.FONT_HERSHEY_SIMPLEX,  # 字体
+            0.9,  # 字体大小
+            (0, 255, 0),  # 字体颜色
+            2,  # 字体粗细
         )
 
-cv2.imshow("Image", img)  # 將IMG 顯示出來
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# 保存绘制了边界框的图像
+cv2.imwrite("annotated_image.jpg", img)
+
+# 使用matplotlib打开保存的图像
+annotated_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # 将图像从BGR转换为RGB
+plt.imshow(annotated_img)
+plt.axis("off")
+plt.show()
